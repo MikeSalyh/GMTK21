acfa8255d05471c229fd5e6b9604ef3c7188da40
mikesalyh <mike@salyh.com>
2021-06-13 07:42:23 -0700

    Start of project. Lil dot follows cursor.

---
+++ GMTK21/Assets/Scripts/CursorFollowLauncher.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorFollowLauncher : MonoBehaviour
+{
+    public GameObject screenCenterPos;
+    public LineRenderer lineRenderer;
+    private Canvas myCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCanvas = GetComponentInParent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, myCanvas.worldCamera, out pos);
+        transform.position = myCanvas.transform.TransformPoint(pos);
+
+        DrawLine();
+    }
+
+    private void LateUpdate()
+    {
+        DrawLine();
+    }
+
+    void DrawLine()
+    {
+        List<Vector3> pos = new List<Vector3>();
+        pos.Add(screenCenterPos.transform.position);
+        pos.Add(transform.position);
+        lineRenderer.startWidth = 1f;
+        lineRenderer.endWidth = 1f;
+        lineRenderer.SetPositions(pos.ToArray());
+        lineRenderer.useWorldSpace = true;
+    }
+}
