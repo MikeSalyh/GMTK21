a382b595253555b0bf1321825acc9e76e24f61e1
mikesalyh <mike@salyh.com>
2021-06-13 10:39:23 -0700

    Main menu

---
+++ GMTK21/Assets/Scripts/Metagame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Metagame : MonoBehaviour
+{
+    private AudioSource src;
+    public AudioClip click;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        src = GetComponent<AudioSource>();
+    }
+
+    public void StartGame()
+    {
+        src.PlayOneShot(click);
+        SceneManager.LoadScene("Gameplay");
+    }
+}
