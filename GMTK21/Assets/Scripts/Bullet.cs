d606b86d8e7b62b9cd98ba5e1d49453a66ce108c
mikesalyh <mike@salyh.com>
2021-06-13 08:34:36 -0700

    Bullet wrapping.

--- GMTK21/Assets/Scripts/Bullet.cs
+++ GMTK21/Assets/Scripts/Bullet.cs
@@ -3,21 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class Bullet : WrappingObject
 {
     public Vector2 velocity;
     public float minLaunchSpeed, maxLaunchSpeed;
-    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
+        base.Update();
         transform.Translate(velocity * Time.deltaTime);
     }
 
---
+++ GMTK21/Assets/Scripts/WrappingObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrappingObject : MonoBehaviour
+{
+    protected RectTransform rect;
+
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        Vector2 newPos = rect.localPosition;
+
+        //if offscreen, wrap around!
+        if (rect.localPosition.y > GameManager.SCREENHEIGHT / 2 + rect.sizeDelta.y)
+        {
+            newPos.y = (-GameManager.SCREENHEIGHT / 2) - rect.sizeDelta.y;
+        }
+        else if (rect.localPosition.y < -GameManager.SCREENHEIGHT / 2 - rect.sizeDelta.y)
+        {
+            newPos.y = (GameManager.SCREENHEIGHT / 2) + rect.sizeDelta.y;
+        }
+
+        if (rect.localPosition.x > GameManager.SCREENWIDTH / 2 + rect.sizeDelta.x)
+        {
+            newPos.x = (-GameManager.SCREENHEIGHT / 2) - rect.sizeDelta.x;
+        }
+        else if (rect.localPosition.x < -GameManager.SCREENWIDTH / 2 - rect.sizeDelta.x)
+        {
+            newPos.x = (GameManager.SCREENWIDTH / 2) + rect.sizeDelta.x;
+        }
+
+        rect.localPosition = newPos;
+    }
+}
